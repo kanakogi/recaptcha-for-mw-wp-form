e1af9b64bd97c5d68739d0d5e88cfbcd82742760
miurakazunori <miura.k1988@gmail.com>
2022-04-14 10:58:00 +0900

    Add null check for $post variable to eliminate warning message on 404 pages

--- controllers/EnqueueController.php
+++ controllers/EnqueueController.php
@@ -17,7 +17,7 @@ class EnqueueController
         global $post;
         $option = get_option(Config::OPTION);
         $site_key = esc_html($option['site_key']);
-        if (has_shortcode($post->post_content, 'mwform_formkey') && !empty($site_key)) {
+        if (!empty($post) && has_shortcode($post->post_content, 'mwform_formkey') && !empty($site_key)) {
             wp_enqueue_script('jquery');
             wp_enqueue_script("recaptcha-script", 'https://www.google.com/recaptcha/api.js?render=' . $site_key, array('jquery'), array(), true);
 
